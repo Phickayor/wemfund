9dfb52429ac9e5e997c274fab2d4b1a8208d998d
Okeshola Samuel <jetawof@gmail.com>
2023-03-11 11:53:01 +0100

    Reviews Responsiveness

--- wemfund/components/Reviews.jsx
+++ wemfund/components/Reviews.jsx
@@ -5,7 +5,7 @@ import React from "react";
 function Reviews() {
   const star = <FontAwesomeIcon icon={faStar} />;
   return (
-    <div className="flex justify-between my-10 text-textcolor">
+    <div className="flex flex-col space-y-10 md:flex-row justify-between my-10 text-textcolor ">
       <div className="w-full mx-auto self-center space-y-4 text-center">
         <h1 className="text-2xl mx-auto w-9/12">The Highest Rating App</h1>
         <h1 className="font-semibold text-3xl font-semibold text-darkblue">
@@ -21,7 +21,7 @@ function Reviews() {
         </h1>
         <h1 className="text-2xl mx-auto w-9/12">5- Star Rating</h1>
       </div>
-      <div className="w-full">
+      <div className="mx-auto md:w-full">
         <img src="ReviewsImage.png" />
       </div>
     </div>
