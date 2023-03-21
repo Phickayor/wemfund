e0438396a16a2aaace154d546f079858313af519
Fikayo Jetawo <jetawof@gmail.com>
2023-03-21 01:43:12 +0100

    reviews component new design

--- wemfund/components/Reviews.jsx
+++ wemfund/components/Reviews.jsx
@@ -5,7 +5,7 @@ import React from "react";
 function Reviews() {
   const star = <FontAwesomeIcon icon={faStar} />;
   return (
-    <div className="flex flex-col space-y-10 md:flex-row justify-between my-10 text-textcolor ">
+    <div className="flex flex-col border-2 mx-auto w-10/12 md:flex-row justify-between my-10 text-textcolor ">
       <div className="w-full mx-auto self-center space-y-4 text-center">
         <h1 className="text-2xl mx-auto w-9/12">The Highest Rating App</h1>
         <h1 className="font-semibold text-3xl font-semibold text-darkblue">
