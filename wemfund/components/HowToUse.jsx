532718c9a14d73047b693c95fb3025aadc11396d
Fikayo Jetawo <jetawof@gmail.com>
2023-03-22 19:16:40 +0100

    minor fix

--- wemfund/components/HowToUse.jsx
+++ wemfund/components/HowToUse.jsx
@@ -2,26 +2,56 @@ import React from "react";
 
 function HowToUse() {
   return (
-    <div className="flex font-[poppins] text-darkblue my-10 mx-auto w-10/12">
-      <div className="flex bg-purple ">
+    <div className="flex font-[poppins] text-darkblue h-96 my-10 mx-auto md:w-10/12 w-11/12">
+      <div className="flex bg-purple  w-full ">
         <img src="/howToUseImage.png" className="self-center" />
       </div>
-      <div className="flex flex-col justify-end w-full py-10">
-        <h1 className="text-center font-bold text-6xl text-black">
+      <div className="flex flex-col md:justify-end  py-10">
+        <h1 className="text-center font-bold md:text-6xl text-black">
           How Trippayer works
         </h1>
         <div className="space-y-8 py-6 mx-auto text-darkblue">
-          <div className=" mx-auto space-y-2">
-            <div className="flex space-x-10 text-3xl ">
+          <div className=" md:mx-auto mx-4 space-y-2">
+            <div className="flex md:space-x-10 space-x-5 md:text-3xl font-semibold ">
               <h1 className="font-bold">01</h1>
               <h1 className="font-semibold">Create an Account</h1>
             </div>
-            <p className=" mx-3 px-12 text-left border-l-4 text-xl">
-              Sign up for an account with <br /> your name, email and phone
+            <p className=" md:mx-3 md:px-12 pl-6 text-left border-l-4 md:text-xl text-xs">
+              Sign up for an account with your name, email and phone
               number.
             </p>
           </div>
-          <div className=" mx-auto  space-y-2">
+          <div className=" md:mx-auto mx-4 space-y-2">
+            <div className="flex md:space-x-10 space-x-5 md:text-3xl font-semibold ">
+              <h1 className="font-bold">01</h1>
+              <h1 className="font-semibold">Create an Account</h1>
+            </div>
+            <p className=" md:mx-3 md:px-12 pl-6 text-left border-l-4 md:text-xl text-xs">
+              Sign up for an account with your name, email and phone
+              number.
+            </p>
+          </div>
+          <div className=" md:mx-auto mx-4 space-y-2">
+            <div className="flex md:space-x-10 space-x-5 md:text-3xl font-semibold ">
+              <h1 className="font-bold">01</h1>
+              <h1 className="font-semibold">Create an Account</h1>
+            </div>
+            <p className=" md:mx-3 md:px-12 pl-6 text-left border-l-4 md:text-xl text-xs">
+              Sign up for an account with your name, email and phone
+              number.
+            </p>
+          </div>
+          <div className=" md:mx-auto mx-4 space-y-2">
+            <div className="flex md:space-x-10 space-x-5 md:text-3xl font-semibold ">
+              <h1 className="font-bold">01</h1>
+              <h1 className="font-semibold">Create an Account</h1>
+            </div>
+            <p className=" md:mx-3 md:px-12 pl-6 text-left border-l-4 md:text-xl text-xs">
+              Sign up for an account with your name, email and phone
+              number.
+            </p>
+          </div>
+          {/* <div className=" mx-auto  space-y-2">
             <div className="flex space-x-10 text-3xl font-semibold">
               <h1 className="font-bold">02</h1>
               <h1>Set Up Your Profile</h1>
@@ -50,7 +80,7 @@ function HowToUse() {
               Sign up for an account with <br /> your name, email and phone
               number.
             </p>
-          </div>
+          </div> */}
         </div>
       </div>
     </div>
