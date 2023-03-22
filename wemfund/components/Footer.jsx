68ac08fc2ccf93fca455203fe6357dc924e877f8
Fikayo Jetawo <jetawof@gmail.com>
2023-03-22 12:32:05 +0100

    Footer component new design

--- wemfund/components/Footer.jsx
+++ wemfund/components/Footer.jsx
@@ -1,37 +1,42 @@
 import React from "react";
-
+import { faInstagram, faFacebook } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 function Footer() {
+  const instagram = (
+    <FontAwesomeIcon icon={faInstagram} className="font-semibold text-4xl" />
+  );
+  const facebook = (
+    <FontAwesomeIcon icon={faFacebook} className="font-semibold text-4xl" />
+  );
   return (
     <div className="bg-darkblue border-white border-t-2 text-white">
-      <div className="flex flex-wrap justify-between text-[poppins] md:px-20 px-10  py-5  ">
-        <div className="my-4">
-          <h1 className="text-2xl font-semibold my-4">Company</h1>
-          <ul className="space-y-4">
-            <li>About Us</li>
-            <li>The Trippayer Blog</li>
-            <li>Join our team</li>
-            <li>Our services</li>
+      <div className="flex justify-between text-[poppins] px-16 py-10">
+        <div className="my-4 w-1/2">
+          <h1 className="text-4xl font-bold my-4">Logo</h1>
+          <p className="my-4 w-11/12 font-normal text-2xl">Trippayer is a payment gateway which can be used to make payment for trips in nigeria</p>
+
+          <div className="flex mt-10 space-x-4">
+            <a href="">{instagram}</a>
+            <a href="">{facebook}</a>
+            <a href="">{instagram}</a>
+            <a href="">{facebook}</a>
+          </div>
+        </div>
+        <div className="my-4 px-10">
+          <h1 className="text-3xl font-semibold my-4">Quick Links</h1>
+          <ul className="space-y-4 font-medium text-2xl">
+            <li>Terms and Conditions</li>
+            <li>Privacy Policy</li>
           </ul>
         </div>
-        <div className="my-4">
-          <h1 className="text-2xl font-semibold my-4">Resources</h1>
-          <ul className="space-y-4">
-            <li>Privacy and Policy</li>
-            <li>Terms Of Services</li>
-            <li>Feedback</li>
-            <li>Security</li>
+        <div className="my-4 px-10">
+          <h1 className="text-3xl font-semibold my-4">Supports</h1>
+          <ul className="text-2xl space-y-4 font-medium">
+            <li>Help</li>
+            <li>Contact us</li>
+            <li>Cookie Policy</li>
           </ul>
         </div>
-        <div className="my-4">
-          <h1 className="text-2xl font-semibold my-4">Address</h1>
-          <p className="mt-4">01 stong avenue, ikeja Lagos State.</p>
-        </div>
-      </div>
-      <div className="md:px-20 px-10 flex space-x-5">
-        <img src="/favicon.ico" />
-        <p className="self-center font-[open sans]">
-          Trippayer &copy; 2023 Trippayer.com
-        </p>
       </div>
     </div>
   );
