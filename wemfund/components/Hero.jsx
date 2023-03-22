48921fc30e347ac324a6cec69d616392203e593a
Fikayo Jetawo <jetawof@gmail.com>
2023-03-22 19:16:15 +0100

    Hero responsiveness

--- wemfund/components/Hero.jsx
+++ wemfund/components/Hero.jsx
@@ -2,12 +2,12 @@ import React from "react";
 function Hero() {
   return (
     <div className="text-darkblue text-center">
-      <div className="flex flex-col justify-center py-4 md:px-10 px-5 ">
+      <div className="flex flex-col justify-center py-4 md:px-10 px-5 my-10">
         <div className="md:w-1/2 px-5 w-5/6 self-center space-y-5">
-          <h1 className="md:text-6xl text-3xl font-semibold md:leading-[72px] leading-[50px] font-[poppins]">
-            Pay Ride <br className="md:hidden" /> With Ease
+          <h1 className="md:text-6xl text-2xl font-semibold md:leading-[72px] font-[poppins]">
+            Pay Ride <br /> With Ease
           </h1>
-          <p className="md:text-3xl text-lg font-[poppins] mx-10">
+          <p className="md:text-3xl text-lg font-medium font-[poppins] md:mx-10">
             With Trippayer you can easily pay for your ride on your mobile phone
             anywhere anytime.
           </p>
@@ -19,14 +19,14 @@ function Hero() {
           />
         </div>
 
-        <p className="font-semibold text-6xl leading-[70px] ">
+        <p className="mt-10 font-semibold text-3xl md:text-6xl md:leading-[70px] ">
           Easy <b className="text-purple font-semibold">Payment for trips </b><br />
           Sign up for <b className="text-purple font-semibold">free </b>now!
         </p>
 
         <div className="flex justify-center my-5 ">
-          <img src="/PlayStore.png" className="md:mx-4 m-2" />
-          <img src="/AppStore.png" className="md:mx-4 m-2" />
+          <img src="/PlayStore.png" className="md:mx-4 m-2 rounded-3xl" />
+          <img src="/AppStore.png" className="md:mx-4 m-2 rounded-3xl" />
         </div>
       </div>
     </div>
--- wemfund/components/Reviews.jsx
+++ wemfund/components/Reviews.jsx
@@ -5,10 +5,10 @@ import React from "react";
 function Reviews() {
   const star = <FontAwesomeIcon icon={faStar} />;
   return (
-    <div className="flex flex-col mx-auto w-10/12 md:flex-row justify-between my-10 text-textcolor ">
-      <div className="w-full mx-auto self-center space-y-4 text-center">
+    <div className="flex flex-col mx-auto w-10/12 md:flex-row my-10 text-textcolor ">
+      <div className="w-1/2 mx-auto self-center space-y-4 text-center">
         <h1 className="text-3xl mx-auto w-9/12">The Highest Rating App</h1>
-        <h1 className="font-semibold text-3xl font-semibold text-darkblue">
+        <h1 className="font-semibold text-3xl text-darkblue">
           Trippayer App
         </h1>
         <h1 className="text-4xl font-bold text-darkblue">99%</h1>
@@ -21,8 +21,8 @@ function Reviews() {
         </h1>
         <h1 className="text-3xl mx-auto w-9/12">5- Star Rating</h1>
       </div>
-      <div className="md:w-full flex justify-end">
-        <img src="ReviewsImage.png" className="" />
+      <div className="md:w-1/2 flex flex-col">
+        <img src="ReviewsImage.png" className="self-center" />
       </div>
     </div>
   );
