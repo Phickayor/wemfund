2caaf601760d3e8b6582ce15b6e626ec7d9bfb3e
Fikayo Jetawo <jetawof@gmail.com>
2023-03-22 12:33:28 +0100

    About component new design

--- wemfund/components/About.jsx
+++ wemfund/components/About.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 function About() {
   return (
-    <div className="my-5 text-textcolor font-[open sans] space-y-4 text-center w-9/12 mx-auto">
-      <h1 className="text-darkblue font-semibold text-4xl my-5">
+    <div className="my-5  font-[poppins] space-y-12 text-center w-9/12 mx-auto">
+      <h1 className="text-darkblue font-semibold text-7xl">
         About Trippayer
       </h1>
-      <p className="text-xl">
+      <p className="text-5xl text-textcolor font-light">
         Sass koscheria. Rear mms:a. Prev geogt. Megass ultrafogen. Dess. Aktiga
         sev. Karl Lindholm gubön. Bihyd besade. Diavasam. Ressade. Ratt sov. Din
         lunyjeskap. /
