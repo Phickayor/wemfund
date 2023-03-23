825aa0e7340ea4286a352381c0eab5149b71ed08
Fikayo Jetawo <jetawof@gmail.com>
2023-03-23 13:45:58 +0100

    Preventing large Fa icons in about page

--- wemfund/pages/about.js
+++ wemfund/pages/about.js
@@ -5,21 +5,27 @@ import NavBar from '@/components/NavBar'
 import Vision from '@/components/Vision'
 import Head from 'next/head'
 import React from 'react'
-
+// The following import prevents a Font Awesome icon server-side rendering bug,
+// where the icons flash from a very large icon down to a properly sized one:
+import '@fortawesome/fontawesome-svg-core/styles.css';
+// Prevent fontawesome from adding its CSS since we did it manually above:
+import { config } from '@fortawesome/fontawesome-svg-core';
+import Head from 'next/head';
+config.autoAddCss = false; /* eslint-disable import/first */
 function about() {
   return (
     <div>
-    <Head>
-      <title>About Trippayer</title>
-      <meta name="description" content="Payment for trips" />
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
-        <NavBar/>
-        <About/>
-        <Vision/>
-        <DownloadApp/>
-        <Footer/>
+      <Head>
+        <title>About Trippayer</title>
+        <meta name="description" content="Payment for trips" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <NavBar />
+      <About />
+      <Vision />
+      <DownloadApp />
+      <Footer />
     </div>
   )
 }
--- wemfund/pages/index.js
+++ wemfund/pages/index.js
@@ -21,12 +21,12 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <NavBar/>
-      <Hero/>
-      <HowToUse/>
-      <Features/>
-      <Reviews/>
-      <Footer/>
+      <NavBar />
+      <Hero />
+      <HowToUse />
+      <Features />
+      <Reviews />
+      <Footer />
     </>
   )
 }
