5489854a35b4ec3b5540c6cd166b62c40024efd1
Fikayo Jetawo <jetawof@gmail.com>
2023-03-23 13:48:01 +0100

    follow up

--- wemfund/pages/about.js
+++ wemfund/pages/about.js
@@ -9,8 +9,7 @@ import React from 'react'
 // where the icons flash from a very large icon down to a properly sized one:
 import '@fortawesome/fontawesome-svg-core/styles.css';
 // Prevent fontawesome from adding its CSS since we did it manually above:
-import { config } from '@fortawesome/fontawesome-svg-core';
-import Head from 'next/head';
+import { config } from '@fortawesome/fontawesome-svg-core'
 config.autoAddCss = false; /* eslint-disable import/first */
 function about() {
   return (
