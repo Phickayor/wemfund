acc23c0001afe659c5142a12d9d6d51a80a78245
Fikayo Jetawo <jetawof@gmail.com>
2023-03-23 12:05:35 +0100

    DownloadApp component responiveness

--- wemfund/components/DownloadApp.jsx
+++ wemfund/components/DownloadApp.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 function DownloadApp() {
   return (
-    <div className="mx-auto w-10/12 text-center my-10">
-      <h1 className="text-6xl font-semibold leading-[52px] font-[poppins] text-darkblue">
+    <div className="mx-auto md:w-10/12 w-11/12 text-center my-10">
+      <h1 className="md:text-6xl text-4xl font-semibold md:leading-[52px] font-[poppins] text-darkblue">
         Download the Trippayer App
       </h1>
-      <p className="text-4xl my-5 mx-auto w-3/5">Join over 200+ people already growing with Trippayer</p>
+      <p className="md:text-4xl text-2xl my-5 mx-auto md:w-3/5">Join over 200+ people already growing with Trippayer</p>
       <button className="bg-darkblue text-white font-medium text-2xl px-10 py-3 mt-5 font-[poppins] rounded-xl">
         Download app
       </button>
