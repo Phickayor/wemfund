75a23e3fa73cc492248bbd19aa111d2d56c5021e
Fikayo Jetawo <jetawof@gmail.com>
2023-03-23 12:41:30 +0100

    minor fixes on reviews component

--- wemfund/components/Reviews.jsx
+++ wemfund/components/Reviews.jsx
@@ -5,21 +5,21 @@ import React from "react";
 function Reviews() {
   const star = <FontAwesomeIcon icon={faStar} />;
   return (
-    <div className="flex flex-col mx-auto w-10/12 md:flex-row my-10 text-textcolor ">
-      <div className="md:w-1/2 mx-auto self-center space-y-4 text-center">
-        <h1 className="text-3xl mx-auto w-10/12">The Highest Rating Payment App</h1>
-        <h1 className="font-semibold text-3xl text-darkblue">
+    <div className="flex flex-col mx-auto md:w-10/12 w-11/12 md:flex-row my-10 text-textcolor ">
+      <div className="md:w-1/2 mx-auto self-center space-y-4 text-center md:py-0 py-5">
+        <h1 className="md:text-3xl text-2xl mx-auto w-10/12">The Highest Rating Payment App</h1>
+        <h1 className="font-semibold md:text-3xl  text-2xl text-darkblue">
           Trippayer App
         </h1>
-        <h1 className="text-4xl font-bold text-darkblue">99%</h1>
-        <h1 className="text-4xl font-bold text-darkblue">
+        <h1 className="md:text-4xl text-3xl font-bold text-darkblue">99%</h1>
+        <h1 className="md:text-4xl text-3xl font-bold text-darkblue">
           {star} &nbsp;
           {star} &nbsp;
           {star} &nbsp;
           {star} &nbsp;
           {star} &nbsp;
         </h1>
-        <h1 className="text-3xl mx-auto w-9/12">5- Star Rating</h1>
+        <h1 className="md:text-3xl text-2xl mx-auto w-9/12">5- Star Rating</h1>
       </div>
       <div className="md:w-1/2 flex flex-col">
         <img src="ReviewsImage.png" className="self-center" />
