d253ffb3e7824f54a33b5af0f4d20edcf63a48e1
Fikayo Jetawo <jetawof@gmail.com>
2023-03-27 15:54:55 +0100

    Footer ui change

--- wemfund/components/Footer.jsx
+++ wemfund/components/Footer.jsx
@@ -24,13 +24,13 @@ function Footer() {
   return (
     <div className="bg-darkblue border-white border-t-2 text-white">
       <div
-        className="hidden md:flex justify-between text-[poppins] px-16 py-10"
+        className="flex flex-wrap md:flex-nowrap justify-between text-[poppins] md:px-16 px-8 py-10"
         data-aos="fade-up"
         data-aos-duration="700"
       >
-        <div className="my-4 w-1/2">
+        <div className="my-4 md:w-1/2 order-3 md:order-1">
           <img src="/lightlogo.png" className="text-4xl font-bold my-4 h-32" alt="Logo" />
-          <p className="my-4 w-11/12 font-normal text-2xl">TripPayer is a payment gateway which can be used to make payment for trips in nigeria</p>
+          <p className="my-4 md:w-11/12 font-normal text-2xl">TripPayer is a payment gateway which can be used to make payment for trips in nigeria</p>
 
           <div className="flex mt-10 space-x-4">
             <a href="https://www.instagram.com/trippayer/">{instagram}</a>
@@ -38,34 +38,20 @@ function Footer() {
             <a href="">{linkedin}</a>
           </div>
         </div>
-        <div className="my-4 px-10">
+        <div className="my-4 md:px-10 px-5 order-1 md:order-2 w-1/2 md:w-fit">
           <h1 className="text-3xl font-semibold my-4">Quick Links</h1>
           <ul className="space-y-4 font-medium text-2xl">
             <li>Terms and Conditions</li>
             <li>Privacy Policy</li>
           </ul>
         </div>
-        <div className="my-4 px-10">
+        <div className="my-4 md:px-10 px-5 order-2 md:order-3 w-1/2 md:w-fit">
           <h1 className="text-3xl font-semibold my-4">Supports</h1>
           <ul className="text-2xl space-y-4 font-medium">
             <li>Help</li>
             <li>Contact us</li>
             <li>Cookie Policy</li>
           </ul>
-        </div>
-      </div>
-      <div className=" md:hidden mx-auto py-4" data-aos="fade-up"
-        data-aos-duration="700">
-        <ul className="flex text-2xl flex-col space-y-10 py-5 ">
-          <li className="self-center">Contact us</li>
-          <li className="self-center">Quick links</li>
-          <li className="self-center" >Support</li>
-        </ul>
-        <div className="flex justify-center mt-2 space-x-6">
-          <a href="https://www.instagram.com/trippayer/">{instagram}</a>
-          <a href="">{twitter}</a>
-          <a href="">{linkedin}</a>
-          <a href="mailto:trippayer@gmail.com">{mail}</a>
         </div>
       </div>
 
